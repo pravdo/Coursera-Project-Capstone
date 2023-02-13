537b5336bdcce6efae7abae88bcb7bf3deeb34c5
Pravdomir Kostov <pravdomir1998@gmail.com>
2023-02-14 01:30:20 +0200

    first components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,47 @@
+import React from "react";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 import logo from "../images/logo.png";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
-    <header>
-      <img src={logo} alt="logo" />
+    <header className="header">
+      <div className="content">
+        <img src={logo} alt="logo" />
+        <div className="title-content">
+          <h1 className="title yellow-text">Little Lemon</h1>
+          <h2 className="headerText sub-title">Chicago</h2>
+        </div>
+        <div className="sub-content">
+          <p className="headerText plain-text">
+            {" "}
+            We are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button
+            aria-label="On Click"
+            onClick={() => navigate("/booking")}
+            className="primaryButton yellow-bg"
+          >
+            <h3>Reserve a table</h3>
+          </button>
+        </div>
+      </div>
+      {/* <LazyLoadImage
+    src='/icons_assets/Mario and Adrian A.jpg'
+    loading='lazy'
+    className='main-img'
+    effect='blur'
+    placeholderSrc='/icons_assets/Mario and Adrian A.jpg'
+    /> */}
+      <img
+        alt="img"
+        className="main-img resImg"
+        loading="lazy"
+        src="/icons_assets/Mario and Adrian A.jpg"
+      />
     </header>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,44 @@
-const Nav = () => {
+import { Link } from "react-router-dom";
+import NavLogo from "../images/nav-logo.png";
+
+const Nav = (props) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/">Reservations</a>
-        </li>
-        <li>
-          <a href="/">Order Online</a>
-        </li>
-        <li>
-          <a href="/">Login</a>
-        </li>
-      </ul>
-    </nav>
+    <menu className={`navbar-menu ${props.device}`}>
+      {props.device === "mobile" ? (
+        ""
+      ) : (
+        <Link to="/">
+          <img
+            src={NavLogo}
+            alt="Little Lemon logo"
+            className="nav-image"
+          ></img>
+        </Link>
+      )}
+      <Link className="hover-effect" to="/">
+        <h1>Home</h1>
+      </Link>
+      <Link className="hover-effect" to="/about">
+        <h1>About</h1>
+      </Link>
+      <a
+        className="hover-effect"
+        href={require("../images/menu.webp")}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h1>Menu</h1>
+      </a>
+      <Link className="hover-effect" to="/reservations">
+        <h1>Reservations</h1>
+      </Link>
+      <Link className="hover-effect" to="/order">
+        <h1>Order</h1>
+      </Link>
+      <Link className="hover-effect" to="/login">
+        <h1>Login</h1>
+      </Link>
+    </menu>
   );
 };
 
